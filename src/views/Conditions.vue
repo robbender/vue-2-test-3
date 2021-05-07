caa6afeb5b4233dc49913216fc2424afeb403c44
RobbenderTT <robert@saastransformations.com>
2021-05-07 13:20:28 -0700

    Added alert updated btn to bs variants

--- src/views/Conditions.vue
+++ src/views/Conditions.vue
@@ -1,10 +1,30 @@
 <template>
     <div>
      <h3>Conditions TEST</h3>
-     <b-button 
+        <b-alert
+        :show="dismissCountDown"
+        dismissible
+        variant="info"
+        @dismissed="dismissCountDown=0"
+        @dismiss-count-down="countDownChanged"
+        >Counter C L E A R E D !</b-alert>
+
+             <!-- This will switch buttons but not using bootstrap vue variants -->
+     <!-- <b-button 
         v-on:click="changeName" 
         v-bind:class="{'hidecolor': !show, 'showcolor' : show}"
-        ref="btnToggle">Show</b-button>
+        ref="btnToggle">Show</b-button> -->
+        <b-button
+            variant="primary"
+            v-on:click="changeName" 
+            v-if="!show"
+            ref="btnToggle">Show</b-button>
+        <b-button
+            variant="outline-primary"
+            v-on:click="changeName(); showClearedAlert()" 
+            v-if="show"
+            ref="btnToggle">Hide</b-button>
+        </b-button>
      <div v-show="show">
          <br>
             <h4>This was hidden before. Click 'Hide' to remove.</h4>
@@ -20,14 +40,22 @@ export default {
   },
   data: () => {
       return {
-          show: false,
+        show: false,
+        dismissSecs: 2,
+        dismissCountDown: 0,
       }
   },
   methods: {
-      changeName() {
-          this.show = !this.show;
-          this.$refs.btnToggle.innerText = this.show?'Hide':'Show';
-      }
+    changeName() {
+        this.show = !this.show;
+        this.$refs.btnToggle.innerText = this.show?'Hide':'Show';
+    },
+    countDownChanged(dismissCountDown) {
+    this.dismissCountDown = dismissCountDown
+    },
+    showClearedAlert() {
+        this.dismissCountDown = this.dismissSecs
+    }
   }
 }
 </script>
