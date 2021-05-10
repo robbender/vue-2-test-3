36a0f563f19e77d224540a383b0d13f860ebec89
RobbenderTT <robert@saastransformations.com>
2021-05-10 15:52:21 -0700

    Updated data

---
+++ src/views/Messages.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h5>{{ title }}</h5>
+        <hr>
+        <div class="user" v-for="(user, id) in users" :key="id">
+            <p>Name: <strong>{{ user.username }}</strong></p>
+            <p>Age: {{ user.age }}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Messages',
+    props: {
+
+    },
+    data: () => {
+        return {
+            title: 'Hello World!',
+            users: [
+                 {username: 'Jimbo', age: 25},
+                 {username: 'Danno', age: 29},
+                 {username: 'Stevo', age: 21},
+                 {username: 'Roberto', age: 35}
+
+                ]
+        }
+    }
+    
+}
+</script>
+<style scoped>
+.user {
+    border: 1px solid #ccc;
+    background-color: lightblue;
+    padding: 10px;
+}
+</style>
